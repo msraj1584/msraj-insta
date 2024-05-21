6352e5944c3c1441c315992c00520604f08b6e36
saranrajmcse <saranrajmcse@gmail.com>
2024-05-21 19:40:43 +0530

    layout

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -31,7 +31,7 @@ export function MobileNav({ className }: { className?: string }) {
           <Menu className="h-4 w-4" />
         </Button>
       </SheetTrigger>
-      <h2>Downloader</h2>
+      <h2> Downloader</h2>
       <SheetContent side="left">
         <h2 className="mb-4 text-center text-lg font-semibold">
           Site Navigation
